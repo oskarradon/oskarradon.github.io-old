94ceb01f8accc0a7a9b1c1f4f027325c5c8da80a
Oskar <oskarradonkimball@gmail.com>
2018-01-08 11:01:52 -0800

    Working rotate animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,45 @@
-let canvas = document.querySelector('#canvas');
-let width = canvas.offsetWidth,
+var canvas = document.querySelector('#canvas');
+var width = canvas.offsetWidth,
     height = canvas.offsetHeight;
 
-let renderer = new THREE.WebGLRenderer({
+var renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true
 });
 
-let scene = new THREE.Scene();
+var scene = new THREE.Scene();
 
-let camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 10000);
-camera.position.set(0, 0 300);
+var camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 10000);
+camera.position.set(0, 0, 300); // center the camera
 
-renderer.setPixelRatio(window.devicePixelRatio > ? 2 : 1); // what does this do?
+var light = new THREE.HemisphereLight(0xffffff, 0x182F63, 0.6);
+light.position.set(200, 300, 400);
+scene.add(light);
+
+var geometry = new THREE.TorusKnotGeometry( 100, 30, 400, 16, 6, 8 );
+
+var material = new THREE.MeshPhongMaterial({
+  emissive: 0xFF4747,
+  emissiveIntensity: 0.4,
+  shininess: 0
+})
+
+var shape = new THREE.Mesh(geometry, material);
+scene.add(shape);
+
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1); // what does this do?
 renderer.setSize(width, height);
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(0xffffff); // bg color
+
+function rotate() {
+  shape.rotation.z = shape.rotation.z + .001;
+  // shape.rotation.x = shape.rotation.x + .01;
+}
 
 function render(a) {
   requestAnimationFrame(render);
-  // call rotate function
+  rotate();
   renderer.render(scene, camera);
 }
+
+requestAnimationFrame(render);
