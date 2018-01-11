3f6c98d34216e98b64c2a4f85ba6cfb04b54d1c6
Oskar <oskarradonkimball@gmail.com>
2018-01-10 23:34:22 -0800

    Begin with footer

---
+++ assets/js/footer.js
@@ -0,0 +1,5 @@
+var cloud = document.querySelector("footer span#cloud"),
+    star = document.querySelector("footer span#star"),
+    flag = document.querySelector("footer span#flag"),
+    peace = document.querySelector("footer span#peace"),
+    smiley = document.querySelector("footer span#smiley");
